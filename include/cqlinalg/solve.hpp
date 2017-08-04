yang2076/ChronusQ_EFP

commit 0e906b6cc0d5a23df21a76d170c403fa32215832

Added stack allocation wrapper around LinSolve, extended this change to DIIS

include/cqlinalg/solve.hpp
@@ -32,6 +32,22 @@ namespace ChronusQ {
    *  \brief Solves a linear system AX = B. Smart wrapper around
    *  DGESV or ZGESV depending on context.
    *
+   *  Assumes preallocated IPIV
+   *
+   *  See http://www.netlib.org/lapack/lapack-3.1.1/html/dgesv.f.html or
+   *      http://www.netlib.org/lapack/lapack-3.1.1/html/zgesv.f.html for
+   *  parameter documentation.
+   */ 
+  template <typename _F>
+  int LinSolve(int N, int NRHS, _F *A, int LDA, _F *B, 
+    int LDB, int* IPIV);
+
+
+
+  /**
+   *  \brief Solves a linear system AX = B. Smart wrapper around
+   *  DGESV or ZGESV depending on context.
+   *
    *  Allocates memory internally through CQMemManager.
    *
    *  See http://www.netlib.org/lapack/lapack-3.1.1/html/dgesv.f.html or
@@ -40,7 +56,35 @@ namespace ChronusQ {
    */ 
   template <typename _F>
   int LinSolve(int N, int NRHS, _F *A, int LDA, _F *B, 
-    int LDB, CQMemManager &mem);
+    int LDB, CQMemManager &mem) {
+
+    int* iPIV = mem.malloc<int>(N);
+
+    LinSolve(N,NRHS,A,LDA,B,LDB,iPIV);
+
+    mem.free(iPIV);
+
+  };
+
+
+  /**
+   *  \brief Solves a linear system AX = B. Smart wrapper around
+   *  DGESV or ZGESV depending on context.
+   *
+   *  Allocates on the stack
+   *
+   *  See http://www.netlib.org/lapack/lapack-3.1.1/html/dgesv.f.html or
+   *      http://www.netlib.org/lapack/lapack-3.1.1/html/zgesv.f.html for
+   *  parameter documentation.
+   */ 
+  template <typename _F>
+  int LinSolve(int N, int NRHS, _F *A, int LDA, _F *B, 
+    int LDB) {
+
+    std::vector<int> iPIV(N,0);
+    LinSolve(N,NRHS,A,LDA,B,LDB,&iPIV[0]);
+
+  };
 
 }; // namespace ChronusQ
 

include/extrapolate.hpp
@@ -92,11 +92,9 @@ namespace ChronusQ {
   template<typename T>
   bool DIIS<T>::extrapolate(){
 
-    int INFO;
     int N          = nExtrap + 1;
     int NRHS       = 1;
     bool InvFail   = false;
-    std::vector<int> iPIV(N,0);
     std::vector<T>   B(N*N,0);
 
     // Build the B matrix
@@ -123,16 +121,7 @@ namespace ChronusQ {
     std::fill_n(&coeffs[0],N,0.);
     coeffs[nExtrap] = -1.0;
  
-    // Solve system of linear equations to get the extrapolation coefficients
-    // TODO: Change to using LinSolve once the memory manager situation is fixed
-    //int INFO = LinSolve(N, 1, B, N, coeffs, N, this->memManager);
-    if(std::is_same<T,dcomplex>::value) {
-      zgesv_(&N,&NRHS,reinterpret_cast<dcomplex*>(&B[0]),&N,&iPIV[0],
-             reinterpret_cast<dcomplex*>(&coeffs[0]),&N,&INFO);
-    } else {
-      dgesv_(&N,&NRHS,reinterpret_cast<double*>(&B[0]),&N,&iPIV[0],
-             reinterpret_cast<double*>(&coeffs[0]),&N,&INFO);
-    }
+    int INFO = LinSolve(N, 1, &B[0], N, &coeffs[0], N);
 
 //  for(auto i = 0ul; i < N; i++)
 //    std::cout << "coeff = " << coeffs[i] << std::endl;

src/cqlinalg/solve.cxx
@@ -28,14 +28,11 @@ namespace ChronusQ {
   // Real wraps DGESV
   template <>
   int LinSolve(int N, int NRHS, double *A, int LDA, double *B, 
-    int LDB, CQMemManager &mem) {
+    int LDB, int *iPIV) {
 
     int INFO;
-    int *iPIV = mem.malloc<int>(N);
 
     dgesv_(&N,&NRHS,A,&LDA,iPIV,B,&LDB,&INFO);
-
-    mem.free(iPIV);
 
     return INFO;
   }; // LinSolve (real)
@@ -44,14 +41,11 @@ namespace ChronusQ {
   // Complex wraps ZGESV
   template <>
   int LinSolve(int N, int NRHS, dcomplex *A, int LDA, 
-    dcomplex *B, int LDB, CQMemManager &mem) {
+    dcomplex *B, int LDB, int* iPIV) {
 
     int INFO;
-    int *iPIV = mem.malloc<int>(N);
 
     zgesv_(&N,&NRHS,A,&LDA,iPIV,B,&LDB,&INFO);
-
-    mem.free(iPIV);
 
     return INFO;
   }; // LinSolve (complex)

commit 8af6d282832ffc3f514f6f1a134411dcd8eeb79d

Fixed LinSolve bug introduced in 0e906b6cc0d5a23df21a76d170c403fa32215832 - All LinSolve wrappers return INFO now

include/cqlinalg/solve.hpp
@@ -60,9 +60,11 @@ namespace ChronusQ {
 
     int* iPIV = mem.malloc<int>(N);
 
-    LinSolve(N,NRHS,A,LDA,B,LDB,iPIV);
+    int INFO = LinSolve(N,NRHS,A,LDA,B,LDB,iPIV);
 
     mem.free(iPIV);
+
+    return INFO;
 
   };
 
@@ -82,7 +84,7 @@ namespace ChronusQ {
     int LDB) {
 
     std::vector<int> iPIV(N,0);
-    LinSolve(N,NRHS,A,LDA,B,LDB,&iPIV[0]);
+    return LinSolve(N,NRHS,A,LDA,B,LDB,&iPIV[0]);
 
   };
 