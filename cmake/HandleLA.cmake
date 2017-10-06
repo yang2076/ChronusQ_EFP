a6a99971274f5541c7e936e3ab8de9f37d54d98b
David Williams-Young <dbwy@uw.edu>
2017-10-06 07:20:29 -0700

    Expose OpenBLAS TARGET through OPENBLAS_TARGET CMake variable, closes liresearchgroup/chronusq_public#9

--- cmake/HandleLA.cmake
+++ cmake/HandleLA.cmake
@@ -6,7 +6,7 @@ message(STATUS "Found Eigen3 -- ${EIGEN3_INCLUDE_DIR}")
 include_directories("${EIGEN3_INCLUDE_DIR}")
 
 if(${CMAKE_CXX_COMPILER} MATCHES "icpc.*$")
-  message(STATUS "INTEL COMPILER")
+  message(STATUS "INTEL COMPILER -- Adding MKL to Compiler Invocation")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mkl=parallel -std=c++11")
   set(_CQ_MKL 1)
 else()
@@ -17,11 +17,20 @@ else()
 
   if( NOT EXISTS "${OPENBLAS_INCLUDEDIR}/f77blas.h" )
 
+
+    if( OPENBLAS_TARGET )
+      message( STATUS "Forcing OpenBLAS TARGET = ${OPENBLAS_TARGET}" )
+      set(OPENBLAS_BUILD_COMMAND make TARGET=${OPENBLAS_TARGET} -j2)
+    else()
+      message( STATUS "Allowing OpenBLAS to determine CPU TARGET" )
+      set(OPENBLAS_BUILD_COMMAND make -j2)
+    endif()
+
     ExternalProject_Add(openblas
       PREFIX ${OPENBLAS_PREFIX}
       URL "${OPENBLAS_PREFIX}/v0.2.19.tar.gz"
       CONFIGURE_COMMAND echo 'No OpenBLAS Configure Command'
-      BUILD_COMMAND make -j2
+      BUILD_COMMAND ${OPENBLAS_BUILD_COMMAND}
       BUILD_IN_SOURCE 1
       INSTALL_COMMAND make install PREFIX=${OPENBLAS_PREFIX} &&
         cd ${OPENBLAS_INCLUDEDIR} && patch < ../patch/lapacke.patch &&
