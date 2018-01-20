5754555bd1577622957b83dd332dd5e77871228e
David Williams-Young <dbwy@uw.edu>
2018-01-20 10:30:42 -0800

    Added a more robust check for Intel compilers in HandleLA.cmake and changed Libint2 build script to allow for restarting the build process in case of interruption

--- cmake/HandleLA.cmake
+++ cmake/HandleLA.cmake
@@ -1,3 +1,24 @@
+#
+# This file is part of the Chronus Quantum (ChronusQ) software package
+# 
+# Copyright (C) 2014-2017 Li Research Group (University of Washington)
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# 
+# Contact the Developers:
+#   E-Mail: xsli@uw.edu
 include(ExternalProject)
 
 
@@ -5,7 +26,7 @@ find_package(Eigen3 REQUIRED)
 message(STATUS "Found Eigen3 -- ${EIGEN3_INCLUDE_DIR}")
 include_directories("${EIGEN3_INCLUDE_DIR}")
 
-if(${CMAKE_CXX_COMPILER} MATCHES "icpc.*$")
+if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
   message(STATUS "INTEL COMPILER -- Adding MKL to Compiler Invocation")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mkl=parallel -std=c++11")
   set(_CQ_MKL 1)
